9ae4f55a5cf9b6fb092fb5e02ef1ffa3da4383c6
Muhammad Mohsin <mohsin@zameen.com>
2021-06-17 11:23:45 +0500

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learna React
+          Learn React
         </a>
       </header>
     </div>
